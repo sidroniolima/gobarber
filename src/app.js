4492265fe798370767576298c7b7f6532cd9dd49
Sidronio Lima <sidronio@sidroniolima.com.br>
2019-08-15 15:53:35 -0300

    Correcoes para inicio do gobarber web

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
@@ -23,9 +24,8 @@ class App {
   }
 
   middleware() {
-    // The request handler must be the first middleware on the app
     this.server.use(Sentry.Handlers.requestHandler());
-
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
